a211fae650e7ea1f54a4e9ce4186afb67c115c4c
Emilse Arias <emilsearias@gmail.com>
2023-06-05 10:20:29 -0300

    vinculamos los items del nav con las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,28 @@ import Error404 from "./components/views/Error404";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/views/Inicio";
+import Registro from "./components/views/Registro";
+import Login from "./components/views/Login";
 import DetalleProducto from "./components/views/DetalleProducto";
 import CrearProducto from "./components/views/producto/CrearProducto";
 import EditarProducto from "./components/views/producto/EditarProducto";
 import Administrador from "./components/views/Administrador";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      {/* <Error404></Error404> */}
-      <Inicio></Inicio>
-      {/* <DetalleProducto></DetalleProducto> */}
-      {/* <CrearProducto></CrearProducto> */}
-      {/* <EditarProducto></EditarProducto> */}
-      {/* <Administrador></Administrador> */}
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/registro" element={<Registro></Registro>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/crear" element={<CrearProducto></CrearProducto>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,19 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink} from 'react-router-dom';
 
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to={'/'}>Cafecito</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Registro</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end className='nav-item nav-link' to={'/'}>Inicio</NavLink>
+            <NavLink end className='nav-item nav-link' to={'/registro'}>Registro</NavLink>
+            <NavLink end className='nav-item nav-link' to={'/administrador'}>Administrador</NavLink>
+            <NavLink end className='nav-item nav-link' to={'/login'}>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
